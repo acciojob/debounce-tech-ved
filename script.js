6770d12ecc60fff685fbecc1214109ac62eae19e
Vedant Dubey <112743896+tech-ved@users.noreply.github.com>
2026-02-18 23:30:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function debounce(callback, delay, immediate = false) {
-  
-  }
+  let timer;
+
+  return function (...args) {
+    const context = this;
+
+    const callNow = immediate && !timer;
+
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      timer = null;
+      if (!immediate) {
+        callback.apply(context, args);
+      }
+    }, delay);
+
+    if (callNow) {
+      callback.apply(context, args);
+    }
+  };
+}
+
   
   module.exports = debounce;
